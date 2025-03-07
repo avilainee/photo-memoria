1eb969f94f53a9338b84cb8ad2df7a32539b1517
avilainee <imbang.marieavilaine@gmail.com>
2025-03-07 14:16:48 +0800

    Update welcome image

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col q-ma-xl">
         <q-card unelevated>
-          <q-card-section> WALA PANG WELCOME HAHAHA </q-card-section>
+          <img src="/icons/logo-128x128.png">
         </q-card>
         <q-btn color="dark" label="Let's take a photo!" class="q-ma-xl" :to="{ path: '/capture' }" unelevated no-caps />
       </div>
